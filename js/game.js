9cea88927929317a949b99a87f5842384ddeca86
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-05-31 14:21:03 +0000

    done 02

--- js/game.js
+++ js/game.js
@@ -1,11 +1,31 @@
 /* global Phaser */ 
 
+// Copyright (c) 2023 Savyon All rights reserved
+//
+// Created by: Savyon
+// Created on: May 2023
+// This file contains the JS functions for index.html
+
+//Game scene
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-}
+  physics : {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+
+  //Set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+  mode: Phaser.Scale.FIT,
+  //place in the middle of page
+  autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+} 
 
 const game = new Phaser.Game(config)
 console.log(game)
