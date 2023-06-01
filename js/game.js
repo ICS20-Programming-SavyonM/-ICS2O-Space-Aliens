18d72f12bc5bf2309596f47e2a71fffcd5511eb3
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-06-01 15:07:43 +0000

    done 06 have to fix 07

--- js/game.js
+++ js/game.js
@@ -10,11 +10,13 @@
 import SplashScene from ".js/splashScene.js"
 import TitleScene from "./titleScene.js"
 import MenuScene from "./menuScene.js"
+import GameScene from './gameScene.js'
 
 //create new scenes
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 const menuScene = new MenuScene()
+const gameScene = new GameScene()
 
 /** 
 * Start game
@@ -43,9 +45,10 @@ const game = new Phaser.Game(config)
 //console.log(game)
 
 //load scenes
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
+game.scene.add('gameScene', gameScene)
 
-//start scene
+// start scene
 game.scene.start("splashScene")
